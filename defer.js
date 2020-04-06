0718d68b5a957cbaf50716b71b3fdd9edc3ffd94
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-04-06 21:04:44 +0200

    Update defer.js

--- defer.js
+++ defer.js
@@ -1,1 +1,9 @@
+Function.prototype.defer = function(ms, ...args) {
+    timeoutID = setTimeout(this, ms, ...args)
+}
 
+let funz = function(a, b) {
+    console.log(a, b);
+}
+
+funz.defer(2000, "buongiorno", "buonasera");
